704ccd67e8b5295156865a61ba7f1e48309d1667
StaticOS <yusuf1905emre1905@gmail.com>
2026-06-10 11:25:15 +0300

    Add AndroidProducts.mk for Static Project configuration

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Static Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/Static_fuxi.mk
+
+COMMON_LUNCH_CHOICES := \
+    Static_fuxi-eng \
+    Static_fuxi-user \
+    Static_fuxi-userdebug
